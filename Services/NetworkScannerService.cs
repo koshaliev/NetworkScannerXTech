3e66c2fe59de2fa3771b97cd5e6896a2d8b28520
koshaliev <default>
2024-11-16 21:29:37 +0300

    Подправил using и добавил комментарий к NetworkScannerService

--- Services/NetworkScannerService.cs
+++ Services/NetworkScannerService.cs
@@ -1,8 +1,6 @@
 ﻿using ManagedNativeWifi;
 using NetworkScannerXTech.Models;
 using NetworkScannerXTech.Shared;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NetworkScannerXTech.Services;
 public class NetworkScannerService : INetworkScannerService
@@ -11,14 +9,15 @@ public class NetworkScannerService : INetworkScannerService
     {
         try
         {
-            await NativeWifi.ScanNetworksAsync(TimeSpan.FromSeconds(1));
-            var scanResult = NativeWifi.EnumerateBssNetworks();
-            
-            var scannedNetworks = scanResult.Select(n => 
-                new WiFiNetwork { 
-                    Bssid = n.Bssid.ToString(), 
-                    Ssid = n.Ssid.ToString(), 
-                    SignalStrength = n.LinkQuality 
+            await NativeWifi.ScanNetworksAsync(TimeSpan.FromSeconds(1)); // производим ручной поиск, чтобы "запустить" wifi-модуль.
+            var scanResult = NativeWifi.EnumerateBssNetworks(); // получаем список сетей
+
+            var scannedNetworks = scanResult.Select(n =>
+                new WiFiNetwork
+                {
+                    Bssid = n.Bssid.ToString(),
+                    Ssid = n.Ssid.ToString(),
+                    SignalStrength = n.LinkQuality
                 }).ToList();
             return Result<IEnumerable<WiFiNetwork>>.Success(scannedNetworks);
         }
